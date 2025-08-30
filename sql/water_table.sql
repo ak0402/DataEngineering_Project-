04ea9b9660e9acb20c17658c3f6aac02b6c83eb3
Akhilesh <65899924+ak0402@users.noreply.github.com>
2025-08-30 21:40:09 +0530

    Update and rename watermark_table.sql to water_table.sql

--- sql/water_table.sql
+++ sql/water_table.sql
@@ -1,9 +1,9 @@
-CREATE TABLE watermark_tabel 
+CREATE TABLE water_tabel 
 (
 	last_load Varchar(2000)
 )
 
 SELECT min(Date_ID) FROM [dbo].[source_cars_data]
 
-INSERT INTO [dbo].[watermark_tabel]
+INSERT INTO [dbo].[water_tabel]
 VALUES('DT00000') 
